24a4543881bb0181e9e8ef612c9706f75084ac24
Akash Sevankar <108569127+akash7kar@users.noreply.github.com>
2023-08-15 13:44:38 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll('.button1,.button2,.button3,.button4');
+const body = document.querySelector("body")
+
+buttons.forEach(function(button){
+    console.log(button);
+    button.addEventListener('click',function(e){
+      console.log(e.target);
+      if(e.target.id === 'grey'){
+        body.style.backgroundColor = e.target.id;
+      }
+      if(e.target.id === 'white'){
+        body.style.backgroundColor = e.target.id;
+      }
+      if(e.target.id === 'blue'){
+        body.style.backgroundColor = e.target.id;
+      }
+      if(e.target.id === 'yellow'){
+        body.style.backgroundColor = e.target.id;
+      }
+    })
+});
